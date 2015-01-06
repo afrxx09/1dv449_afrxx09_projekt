fca576c331838dec1ac10e64c0275ebc5608eb5d
Andreas Fridlund <afrxx09@student.lnu.se>
2015-01-06 08:38:45 +0100

    First

---
+++ controllers/auth.js
@@ -0,0 +1,17 @@
+var passport = require('passport');
+var BasicStrategy = require('passport-http').BasicStrategy;
+var User = require('../models/user');
+
+passport.use(new BasicStrategy(function(username, password, cb){
+	User.findOne({username: username}, function(err, user){
+		if(err) return cb(err);
+		if(!user) return cb(null, false);
+		user.verifyPassword(password, function(err, isMatch){
+			if(err) return cb(err);
+			if(!isMatch) return cb(null, false);
+			return cb(null, user);
+		});
+	});
+}));
+
+exports.isAuthenticated = passport.authenticate('basic', {session: false});
---
+++ server.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var beerController = require('./controllers/beer');
+var userController = require('./controllers/user');
+var passport = require('passport');
+var authController = require('./controllers/auth');
+
+mongoose.connect('mongodb://localhost:27017/beer');
+
+var app = express();
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+app.set('view engine', 'ejs');
+
+
+var staticRouter = express.Router();
+staticRouter.use(function(req, res, next) {
+    console.log(req.method, req.url);
+    next(); 
+});
+staticRouter.get('/', function(req, res){
+	res.render('index');
+});
+
+var apiRouter = express.Router();
+apiRouter.use(function(req, res, next) {
+    console.log(req.method, req.url);
+    next(); 
+});
+apiRouter.route('/beers')
+	.post(authController.isAuthenticated, beerController.postBeers)
+	.get(authController.isAuthenticated, beerController.getBeers);
+apiRouter.route('/beer/:beer_id')
+	.get(authController.isAuthenticated, beerController.getBeer)
+	.put(authController.isAuthenticated, beerController.putBeer)
+	.delete(authController.isAuthenticated, beerController.deleteBeer);
+
+apiRouter.route('/users')
+	.post(userController.postUsers)
+	.get(authController.isAuthenticated, userController.getUsers);
+
+app.use('/api', apiRouter);
+app.use('/', staticRouter);
+
+
+
+app.listen(3000);
+console.log('Server started on port: ' + 3000);
