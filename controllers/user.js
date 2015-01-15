037a514f878094f24ce5f0bc99b0cf0e5643eb5a
afrxx09 <afrxx09@student.lnu.se>
2015-01-15 09:53:30 +0100

    ..

--- controllers/user.js
+++ controllers/user.js
@@ -3,28 +3,23 @@ var User = require('../models/user');
 exports.getProfile = function(req, res){
 	User.findOne({_id: req.user._id}, function(err, user){
 		if(err) res.send(err);
-		res.render('profile');
+		res.render('profile', { message: req.flash('loginMessage')});
 	});
 };
 
 exports.saveProfile = function(req, res){
-	User.findOne({_id: req.user._id}, function(err, user){
-		if(err) res.send(err);
-		if(!user) res.send(null, false);
-		user.firstName = req.body.firstName;
-		user.lastName = req.body.lastName;
-		user.save(function(err){
-			if(err)  res.send(err)
-			res.render('profile');
+	process.nextTick(function(){
+		User.findOne({_id: req.user._id}, function(err, user){
+			if(err) res.send(err);
+			if(!user) res.send(null, false);
+			user.firstName = req.body.firstName;
+			user.lastName = req.body.lastName;
+			user.save(function(err){
+				if(err) res.send(err);
+				res.render('profile');
+			});
 		});
 	});
-};
-
-exports.signup = function(req, res, next, passport){
-	passport.authenticate('local-signup', {
-		successRedirect: '/',
-		failureRedirect: '/signup'
-	})
 };
 
 exports.signupForm = function(req, res){
